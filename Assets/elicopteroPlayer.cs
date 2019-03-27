45259a1aff767b53bc443c5a951aaf78439b3960
Jhonata Rebouças <jhonatareboucas@gmail.com>
2019-03-26 22:37:23 -0300

    Física
    Inclusão da física no elicóptero

---
+++ Assets/elicopteroPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elicopteroPlayer : MonoBehaviour
+{
+    bool inicioJogo = false;
+    bool fimJogo = false; 
+    Vector2 forcaImpulso= new Vector2(0,300);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && !fimJogo){
+            if(!inicioJogo){
+                inicioJogo = true;
+                GetComponent<Rigidbody2D>().simulated = true;
+            }
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().AddForce(forcaImpulso);
+        }
+    }
+}
---
+++ Assets/scriptMoveFundo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptMoveFundo : MonoBehaviour
+{
+    public float velocidadeCenario = -0.5f;
+    public float limiteCenario = -10f;
+    public float spawCenario = 10;
+    public float alturaCenario = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadeCenario,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x <= limiteCenario){
+            transform.position = new Vector2(spawCenario,alturaCenario);
+        }
+    }
+}
